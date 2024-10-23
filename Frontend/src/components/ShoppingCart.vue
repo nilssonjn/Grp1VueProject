14549164403edd18e763c8e9ec7cfebeae16f1e6
august <august.ryd@gmail.com>
2024-10-23 14:12:15 +0200

    add @click.stop to the buttons in ShoppingCart

--- Frontend/src/components/ShoppingCart.vue
+++ Frontend/src/components/ShoppingCart.vue
@@ -141,8 +141,8 @@ const totalPrice = computed(() => {
 
       <li v-for="book in bookCounts" :key="book.id">
         <h2>{{ book.title }} ({{ book.count }})</h2>
-        <button @click="removeBook(book.id)"> - </button>
-        <AddToCartButton :book="book" :stock="book.stock" @add-to-cart="updateCart">
+        <button @click.stop="removeBook(book.id)"> - </button>
+        <AddToCartButton @click.stop :book="book" :stock="book.stock" @add-to-cart="updateCart">
           +
         </AddToCartButton>
 
