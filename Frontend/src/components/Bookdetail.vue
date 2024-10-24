28e167f5a52be393551bc330b9cdbb41432542a7
jnnilsson <jn.jensnilsson@gmail.com>
2024-10-24 14:16:13 +0200

    fixed positioning and add our book image
    Co-authored-by: August Rydnell <august.rydnell@gmail.com>
    Co-authored-by: Tobias Sjöström <tobias.sjostrom@iths.se>

--- Frontend/src/components/Bookdetail.vue
+++ Frontend/src/components/Bookdetail.vue
@@ -81,37 +81,40 @@ onMounted(async () => {
 
 
 <template>
-  <div class="bg-gray-700 py-8">
+  <div class="bg-white py-8">
     <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
       <div v-if="book" class="flex flex-col md:flex-row -mx-4">
         <div class="md:flex-1 px-4">
-          <div class="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
-            <img class="w-full h-full object-cover"
+          <div class="h-[460px] rounded-lg bg-gray-100 border-lg mb-4">
+            <img class="w-full h-full object-contain"
                  :src="book.image" alt="book cover">
           </div>
-          <div class="flex -mx-2 mb-4">
-            <div class="w-1/2 px-2">
+
+        </div>
+        <div class="md:flex-1 px-4">
+          <h2 class="text-2xl font-bold text-black mb-2">{{ book.title }}</h2>
+          <div class="flex mb-4">
+            <div class="mr-4">
+              <span class="font-bold text-black">Price: </span>
+              <span class="text-black">{{ book.price }} kr</span>
+            </div>
+            <div>
+              <span class="font-bold text-black">Availability:</span>
+              <span class="text-black"> {{ book.stock }}</span>
+            </div>
+          </div>
+
+          <div class="flex mx-0 mb-4">
+            <div class="w-full px-0">
               <AddToCartButton
                   :book="book"
                   @add-to-cart="addToCart"/>
             </div>
           </div>
-        </div>
-        <div class="md:flex-1 px-4">
-          <h2 class="text-2xl font-bold text-gray-800 dark:text-white mb-2">{{ book.title }}</h2>
-          <div class="flex mb-4">
-            <div class="mr-4">
-              <span class="font-bold text-gray-700 dark:text-gray-300">Price: </span>
-              <span class="text-gray-600 dark:text-gray-300">{{ book.price }} kr</span>
-            </div>
-            <div>
-              <span class="font-bold text-gray-700 dark:text-gray-300">Availability:</span>
-              <span class="text-gray-600 dark:text-gray-300"> {{ book.stock }}</span>
-            </div>
-          </div>
+
           <div>
-            <span class="font-bold text-gray-700 dark:text-gray-300">Book summary </span>
-            <p class="text-gray-600 dark:text-gray-300 text-sm mt-2">
+            <span class="font-bold text-black">Book summary </span>
+            <p class="text-black text-sm mt-2">
               {{ book.summary }}
             </p>
           </div>
