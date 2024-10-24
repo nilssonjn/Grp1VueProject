4206dbf3952397111b78787cf0b0dbc2a348ecce
jnnilsson <jn.jensnilsson@gmail.com>
2024-10-24 12:08:34 +0200

    change template in Bookdetail.vue
    Co-authored-by: August Rydnell <august.rydnell@gmail.com>
    Co-authored-by: Tobias Sjöström <tobias.sjostrom@iths.se>

--- Frontend/src/components/Bookdetail.vue
+++ Frontend/src/components/Bookdetail.vue
@@ -45,56 +45,77 @@ onMounted(async () => {
 </script>
 
 <!--<template>-->
-<!--  <div v-if="book">-->
-<!--    <img :src="book.image" alt="book cover" />-->
-<!--    <AddToCartButton-->
-<!--        :book="book"-->
-<!--        :stock="book.stock"-->
-<!--        @add-to-cart="addToCart"-->
-<!--    />-->
-<!--    <h1>{{ book.title }}</h1>-->
-<!--    <p>{{ book.author || 'Ingen författare tillgänglig' }}</p>-->
-<!--    <p>Lagerstatus: {{book.stock}}</p>-->
-<!--    <p>Publiceringsår: {{ book.publishyear || 'Inget publiceringsdatum tillgänglig' }}</p>-->
-<!--    <p>ISBN:{{book.isbn}}</p>-->
-<!--    <p>Pris: {{book.price}}</p>-->
-<!--    <p>{{ book.summary }}</p>-->
-<!--  </div>-->
-<!--  <div v-else>-->
-<!--    <p>Laddar bokinformation...</p>-->
-<!--  </div>-->
+<!--  <section id="Bookdetail"-->
+<!--           class="w-fit mx-auto grid grid-cols-1-->
+<!--           justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">-->
+<!--    <div class="w-90">-->
+<!--      <div v-if="book">-->
+<!--        <img :src="book.image" alt="book cover" class="h-82 w-72 object-cover px-4 py-3"/>-->
+<!--        <div class="px-4 py-3 w-90">-->
+<!--          <AddToCartButton-->
+<!--              :book="book"-->
+<!--              :stock="book.stock"-->
+<!--              @add-to-cart="addToCart"-->
+<!--          />-->
+<!--          -->
+<!--          <p class="text-lg font-bold text-black truncate block capitalize py-2">{{ book.title }}</p>-->
+<!--          <div class="bg-gray-100 p-4 rounded-md">-->
+<!--            <p>Author: {{ book.author || 'Ingen författare tillgänglig' }}</p>-->
+<!--            <p>Available: {{ book.stock }}</p>-->
+<!--            <p>Published: {{ book.publishyear || 'Inget publiceringsdatum tillgänglig' }}</p>-->
+<!--            <p>ISBN: {{ book.isbn }}</p>-->
+<!--            <p>Price: {{ book.price }} kr</p>-->
+<!--          </div>-->
+<!--          <br>-->
+<!--          <div class="bg-gray-100 p-4 rounded-md">-->
+<!--            <p>Summary: {{ book.summary }}</p>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--      <div v-else>-->
+<!--        <p>Laddar bokinformation...</p>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </section>-->
 <!--</template>-->
 
 <template>
-  <section id="Bookdetail"
-           class="w-fit mx-auto grid grid-cols-1
-           justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
-    <div class="w-90">
-      <div v-if="book">
-        <img :src="book.image" alt="book cover" class="h-82 w-72 object-cover px-4 py-3"/>
-        <div class="px-4 py-3 w-90">
-          <AddToCartButton
-              :book="book"
-              :stock="book.stock"
-              @add-to-cart="addToCart"
-          />
-          <p class="text-lg font-bold text-black truncate block capitalize py-2">{{ book.title }}</p>
-          <div class="bg-gray-100 p-4 rounded-md">
-            <p>Author: {{ book.author || 'Ingen författare tillgänglig' }}</p>
-            <p>Available: {{ book.stock }}</p>
-            <p>Published: {{ book.publishyear || 'Inget publiceringsdatum tillgänglig' }}</p>
-            <p>ISBN: {{ book.isbn }}</p>
-            <p>Price: {{ book.price }} kr</p>
+  <div class="bg-gray-700 py-8">
+    <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex flex-col md:flex-row -mx-4">
+        <div class="md:flex-1 px-4">
+          <div class="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
+            <img class="w-full h-full object-cover"
+                 src="https://cdn.pixabay.com/photo/2020/05/22/17/53/mockup-5206355_960_720.jpg" alt="Product Image">
           </div>
-          <br>
-          <div class="bg-gray-100 p-4 rounded-md">
-            <p>Summary: {{ book.summary }}</p>
+          <div class="flex -mx-2 mb-4">
+            <div class="w-1/2 px-2">
+              <AddToCartButton
+                  :book="book"
+                  @add-to-cart="addToCart"/>
+            </div>
+          </div>
+        </div>
+        <div class="md:flex-1 px-4">
+          <h2 class="text-2xl font-bold text-gray-800 dark:text-white mb-2">{{ book.title }}</h2>
+          <div class="flex mb-4">
+            <div class="mr-4">
+              <span class="font-bold text-gray-700 dark:text-gray-300">Price: </span>
+              <span class="text-gray-600 dark:text-gray-300">{{ book.price }} kr</span>
+            </div>
+            <div>
+              <span class="font-bold text-gray-700 dark:text-gray-300">Availability:</span>
+              <span class="text-gray-600 dark:text-gray-300"> {{ book.stock }}</span>
+            </div>
+          </div>
+          <div>
+            <span class="font-bold text-gray-700 dark:text-gray-300">Book summary </span>
+            <p class="text-gray-600 dark:text-gray-300 text-sm mt-2">
+              {{ book.summary }}
+            </p>
           </div>
         </div>
       </div>
-      <div v-else>
-        <p>Laddar bokinformation...</p>
-      </div>
     </div>
-  </section>
+  </div>
 </template>
