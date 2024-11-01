2587e8f17cc98ffe1c6938afc4930248af885965
jnnilsson <jn.jensnilsson@gmail.com>
2024-11-01 12:28:53 +0100

    .

--- Frontend/src/components/ShoppingCart.vue
+++ Frontend/src/components/ShoppingCart.vue
@@ -129,7 +129,8 @@ const totalPrice = computed(() => {
           <h3 class="text-gray-900 font-semibold">{{ book.title }} ({{ book.count }})</h3>
           <p class="text-gray-700 mt-1">{{ book.price }} kr</p>
         </div>
-        <button @click.stop="removeBook(book.id)" class="ml-auto py-2 px-4 bg-red-500 hover:bg-red-600 text-white rounded">
+        <button @click.stop="removeBook(book.id)"
+                class="ml-auto py-2 px-4 bg-red-500 hover:bg-red-600 text-white rounded">
           -
         </button>
         <AddToCartButton @click.stop :book="book" :stock="book.stock" @add-to-cart="updateCart" class="py-2 px-4">
